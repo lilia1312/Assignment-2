bc1be83a589b52b5b80c4f18a77cfa5acc5458e4
Treasure <155763708+lilia1312@users.noreply.github.com>
2024-08-06 13:37:59 -0500

    Update to thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -8,9 +8,6 @@
 
 ****************/
 
-
-
-function order_summary(){}
 $items = [
     ['index' => 1, 'name' => 'MacBook', 'price' => 1899.99, 'quantity' => 0],
     ['index' => 2, 'name' => 'Razer Gaming Mouse', 'price' => 79.99, 'quantity' => 0],
@@ -19,75 +16,76 @@ $items = [
     ['index' => 5, 'name' => 'Footpedal', 'price' => 119.99, 'quantity' => 0]
 ];
 
-function validate_input() {
+ function validate_input() {
     $error = false;
 
-    // // email validation
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    if (!$email) {
+    //email validation
+    $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+    if(!$email){
         $error = true;
     }
 
-
-    // // Canadian postal code validation
-    $postalCode = filter_input(INPUT_POST, 'postal', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-Z][0-9]$/')));     
-    if (!$postalCode) {   
-        $error = true;     
-    }
-
-    // // Credit Card number (checking if it is numeric and of valid length)
-    $cardNumber = filter_input(INPUT_POST, 'cardnumber', FILTER_SANITIZE_NUMBER_INT);
-    if (!$cardNumber) {
+    //Canadian postal code validation
+    $postalCode = filter_input(INPUT_POST, 'postal', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-Z][0-9]$/')));
+    if (!$postalCode) {
         $error = true;
     }
 
-    // // Credit Card month
-    $card_month = filter_input(INPUT_POST, 'month', FILTER_SANITIZE_NUMBER_INT);
-    if ($card_month < 1 || $card_month > 12) {
+    //Credit Card number
+    $cardNumber = filter_input(INPUT_POST,'cardnumber', FILTER_SANITIZE_NUMBER_INT);
+    $cardLength = strlen($_POST['cardnumber']);
+    if($cardLength > 10 || $cardLength < 1){
         $error = true;
     }
 
-    // // Credit card year
+    //Credit Card month
+    $month = filter_input(INPUT_POST, 'month', FILTER_SANITIZE_NUMBER_INT);
+    if($month < 1 || $month > 12){
+        $error = true;
+    } 
+
+    //Credit card year
     $card_year = filter_input(INPUT_POST, 'year', FILTER_SANITIZE_NUMBER_INT);
     $currentYear = date('Y');
-    if ($card_year < $currentYear || $card_year > $currentYear + 5) {
+    if($card_year < $currentYear || $card_year > $currentYear + 5){
+        $error = true;
+    }  
+
+    //Card Type
+    $cardType = isset($_POST['cardtype'])? $_POST['cardtype'] : null;
+    if(!$cardType){
         $error = true;
     }
 
-    // Card Type
-    $cardType = isset($_POST['cardtype']);
-    if(!$cardType) {
-        $error = true;
-    }
-
-    // Card info
+    //card info
     $requiredFields = array('fullname', 'address', 'city', 'province', 'cardname');
-    foreach ($requiredFields as $requiredField) {
-        if (empty($_POST[$requiredField])){
+    foreach ( $requiredFields as $requiredField ) {
+        if(empty(trim($_POST[$requiredField]))){
             $error = true;
         }
     }
 
-    // Province
-    $province = $_POST['province'] ?? null;
-    $abbrProvinces = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'ON', 'PE', 'QC', 'SK', 'NT', 'NU', 'YT'];
-    if (!in_array($province, $abbrProvinces)) {
-        $error = true;
+    //province
+    $province = $_POST['province'];
+    $abbrProvinces = ['AB','BC','MB','NB','NL','NS','ON','PE','QC','SK','NT','NU','YT'];
+    if (!in_array($province, $abbrProvinces)){
+        $error= true;
     }
 
-    // Quantities
-    $quantities = ['qty1', 'qty2', 'qty3', 'qty4', 'qty5'];
-    foreach ($quantities as $field) {
-      $quantity = filter_input(INPUT_POST, $field, FILTER_VALIDATE_INT);
-        if ($quantity === false && $_POST[$field] !== '') {
+    //Validate quantities
+    $quantities = ['qty1','qty2', 'qty3', 'qty4', 'qty5'];
+    foreach ($quantities as $quantityField){
+        $quantity = filter_input(INPUT_POST, $quantityField, FILTER_VALIDATE_INT);
+        if($quantity === false && $_POST[$quantityField] !== ''){
             $error = true;
         }
     }
 
     return $error;
-}
+}   
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -136,27 +134,28 @@ function validate_input() {
   
   <table>
     <tbody>
-    <?php 
-    for($i = 1; $i <= 5; $i++){
-        $quantity = $_POST["qty$i"];
-        if($quantity > 0) {
-            $description = $items[$i - 1]["name"];
-            $cost = $items[$i - 1]["price"] * $quantity;
+        <?php 
+            for($i = 1; $i <= 5; $i++){
+                $quantity = $_POST["qty$i"];
+                if($quantity > 0) {
+                    $description = $items[$i - 1]["name"];
+                    $cost = $items[$i - 1]["price"] * $quantity;
 
-            $output = "<tr>
-                        <td>$quantity</td>
-                        <td>$description</td>
-                        <td>$cost</td>
-                    </tr>";
-            echo $output;
-        }
-    }
-?>
-    </tbody>
-  </table>
-  </div>
+                    $output = "<tr>
+                                <td>$quantity</td>
+                                <td>$description</td>
+                                <td>$cost</td>
+                            </tr>";
+                    echo $output;
+                }
+            }
+        ?>
+   </tbody>
+</table>
     <?php else:?>
-        <h2> Error</h2>
-    <?php endif ?>
+        <h2>Error</h2>
+    <?php endif?>
+ 
+</div>
 </body>
 </html>
